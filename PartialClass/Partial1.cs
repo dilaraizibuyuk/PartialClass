fedf41b1ca529596263197614376c40d80a6f8bb
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-13 17:57:46 +0300

    Proje dosyası ekle.

---
+++ PartialClass/Partial1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClass
+{
+    public partial class Partial1
+    {
+        public void MesajYazdir1()
+        {
+            Console.WriteLine("Hey!!");
+        }
+    }
+    public partial class Partial1
+    {
+        public void MesajYazdir2()
+        {
+            Console.WriteLine("Ben");
+        }
+    }
+    public partial class Partial1
+    {
+        public void MesajYazdir3()
+        {
+            Console.WriteLine("Partial Class'ım!!");
+        }
+    }
+}
---
+++ PartialClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Partial1 deneme = new Partial1();
+            deneme.MesajYazdir1();
+            deneme.MesajYazdir2();
+            deneme.MesajYazdir3();
+            Console.ReadLine();
+        }
+    }
+}
